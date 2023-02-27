238b3df7ab3e5f5a8957416fa295fd4e287979c1
mark-casias <mark.casias@kanopi.com>
2023-02-27 12:31:26 -0700

    show updates

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,8 +1,9 @@
 <h1>Greetings from The Randomatics</h1>
 
+<img src="images/lazy-lizard-april-1.png" alt="psycadelic band pic at the lazy lizard" />
+
 <h2>Upcoming Shows</h2>
 <ul>
-  <li>Feb 25th: Lazy Lizard Grill</li>
   <li>March 26th: New Mexico Guitar Show</li>
   <li>April 1st: Lazy Lizard Grill</li>
 </ul>
