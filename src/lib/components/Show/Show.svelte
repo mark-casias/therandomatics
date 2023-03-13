cdffb45521fa50071750b12a9411ab47e99b2d30
mark-casias <mark.casias@kanopi.com>
2023-03-13 10:12:16 -0600

    Adding Shows and components

---
+++ src/lib/components/Show/Show.svelte
@@ -0,0 +1,44 @@
+<script>
+  export let date;
+  export let place;
+  export let time;
+  export let image;
+</script>
+
+<div class="show">
+  <div class="show__date">{date} {time}</div>
+  <div class="show__place">
+  {#if image}
+    <img src={image.src} alt={image.alt} />
+  {:else}
+    {place}
+  {/if}
+  </div>
+</div>
+
+<style lang="scss">
+  .show {
+    margin: 0 2rem;
+    height: 100%;
+    border: 1px solid red;
+    div {
+      padding: 1rem;
+    }
+
+    img {
+      max-width: 300px;
+    }
+
+    &__date {
+      background-color: darkturquoise;
+      color: black;
+    }
+    &__place {
+      font-size: 2rem;
+    }
+    &__time {
+      background-color: black;
+      color: white;
+    }
+  }
+</style>
--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,11 +1,29 @@
+<script>
+import Show from "../lib/components/Show/Show.svelte";
+
+const llimage = {
+  src: "images/lazy-lizard-april-1.png",
+  alt: "The Randomatics are playing at The Lazy Lizard on April 1st starting at 6pm.",
+};
+const nmimage = {
+  src: "images/nm-guitar-show-2023.jpg",
+  alt: "the band is playing at the NM guitar show on March 26th at 4pm"
+}
+const mvimage = {
+  src: "images/monte-vista-march-2023.jpg",
+  alt: "the band is playing at the Monte Vista Fire Station on March 31st starting at 7pm"
+}
+</script>
+
 <h1>Greetings from The Randomatics</h1>
 
-<img src="images/lazy-lizard-april-1.png" alt="psycadelic band pic at the lazy lizard" />
+<p>With such reviews as, "I don't know, I don't think they'd be very good for a kids party", and "Sorry, what are they called?...Why?", it's not hard to understand why The Randomatics are one of Albuquerque's bands. The Randomatics are as fun, festive, and fantastic as the state that bore them.</p>
 
 <h2>Upcoming Shows</h2>
-<ul>
-  <li>March 26th: New Mexico Guitar Show</li>
-  <li>April 1st: Lazy Lizard Grill</li>
+<ul class="upcoming">
+  <li><Show place="NM Guitar Show" date="March 26th" time="4pm" image={nmimage}/></li>
+  <li><Show place="Montevista Fire Station" date="March 31st" time="7 - 10p" image={mvimage} /></li>
+  <li><Show place="Lazy Lizard Grill" date="April 1st" time="6 - 9:30p" image={llimage}/></li>
 </ul>
 
 <style lang="scss" global>
@@ -15,4 +33,15 @@
       background-color: black;
     }
   }
+
+  .upcoming {
+    display: flex;
+    justify-content: center;
+    list-style: none;
+
+    li {
+      align-self: stretch;
+    }
+  }
+  p {color: white}
 </style>
