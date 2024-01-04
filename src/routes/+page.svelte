448f04f61d9f9d3709f848bc7e72dd02cabd366f
mark-casias <mark.casias@kanopi.com>
2024-01-04 16:59:38 -0700

    Adding hopper and groundhogs

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,39 +1,19 @@
 <script>
 import Show from "../lib/components/Show/Show.svelte";
 
-const alvaradoimage = {
-  src: "images/alvarado-park-7-13-23.jpg",
-  alt: "The Randomatics at Thursday Food Truck Fest on July 13th"
-}
-
-const lizard0823 = {
-  src: "images/lazy-lizard-08-12-23.jpg",
-  alt: "The Randomatics return to the Lazy Lizard on August 12th"
-}
-
-const lizard1123 = {
-  src: "images/lazy-lizard-11-11-23.jpg",
-  alt: "The anniversary of our first show at the Lazy Lizard"
-}
-
-const mollys0909 = {
-  src: "images/rando-mollys.jpg",
-  alt: "The Randomatics first show at Molly's Bar and Grill on Setpember 9th"
-}
-
-const brewlab081023 = {
-  src: "images/brewlab-8-10-23.png",
-  alt: "The Randomatics at Brewlab in Rio Rancho on August 10th"
-}
-
 const lizard240127 = {
   src: "images/ll-24-01-27.jpg",
   alt: "The Randomatics return to Lazy Lizard on January 27th"
 };
 
 const thirsty240202 = {
-  src: "images/thirsty-eye-24-02-02.jpg",
+  src: "images/20240202--thirsty-eye-groundhog.png",
   alt: "The Randomatics first show at Thirsty Eye on February 2nd"
+}
+
+const hopper240503 = {
+  src: "images/20240503--the-hopper.png",
+  alt: "The Randomatics at The Hopper on May 3rd"
 }
 </script>
 
@@ -45,6 +25,7 @@ const thirsty240202 = {
 <ul class="upcoming">
   <Show place="Lazy Lizard" date="01/27/2024" time="18:00:00" image={lizard240127} />
   <Show place="Thirsty Eye Brewing" date="02/02/2024" time="18:00:00" image={thirsty240202} />
+  <Show place="The Hopper" date="05/03/2024" time="18:00:00" image={hopper240503} />
 </ul>
 
 <style lang="scss">
