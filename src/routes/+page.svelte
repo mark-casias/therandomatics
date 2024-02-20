608e2705f749964bbd1db035c4f7cb9abc602e33
Mark Casias <markie@teampoop.com>
2024-02-19 23:10:11 -0700

    adding various lizard shows

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,16 +1,6 @@
 <script>
 import Show from "../lib/components/Show/Show.svelte";
 
-const kactus20240215 = {
-  src: "images/kaktus-brewing-2.jpg",
-  alt: "The Randomatics at Kaktus Brewing on February 15th."
-};
-
-const bowling240218 = {
-  src: "images/20240218-holiday_bowl.jpg",
-  alt: "The Randomatics at Holiday Bowl!"
-};
-
 const thirsty240405 = {
   src: "images/thirsty_eye_april_poster.jpg",
   alt: "The Randomatics return to Thirsty Eye"
@@ -20,6 +10,16 @@ const hopper240503 = {
   src: "images/hopper-2.jpg",
   alt: "The Randomatics at The Hopper on May 3rd"
 }
+
+const lizardtail240420 = {
+  src: "images/lizard_tail_poster_5.jpg",
+  alt: "The Randomatics at Lizard Tail Brewing"
+}
+
+const lazylizard240511 = {
+  src: "images/lazy-lizard-240511.jpg",
+  alt: "The Randomatics at Lazy Lizard Brewing"
+}
 </script>
 
 <h1>Greetings from The Randomatics</h1>
@@ -28,10 +28,10 @@ const hopper240503 = {
 
 <h2>Upcoming Shows</h2>
 <ul class="upcoming">
-  <Show place="Kaktus Brewing" date="02/15/2024" time="18:30:00" image={kactus20240215} />
-  <Show place="Skidmore's Holiday Bowl" date="02/18/2024" time="14:00:00" image={bowling240218} />
-  <Show place="Thirsty  Eye Brewing" date="04/05/2024" time="19:00:00" image={thirsty240405} />
+  <Show place="Thirsty Eye Brewing" date="04/05/2024" time="19:00:00" image={thirsty240405} />
+  <Show place="Lizard Tail Brewing" date="04/20/2024" time="19:00:00" image={lizardtail240420} />
   <Show place="The Hopper" date="05/03/2024" time="18:00:00" image={hopper240503} />
+  <Show place="Lazy Lizard Brewing" date="05/13/2024" time="18:00:00" image={lazylizard240511} />
 </ul>
 
 <style lang="scss">
